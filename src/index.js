b571ea0926fba03fec1ebf1933eb90941a7ba187
191078834 <191078834@qq.com>
2022-09-13 16:31:50 +0900

    userefs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TextInputWithFocusButton from './TextInputWithFocusButton';
 import Todo from'./Todo';
 import TodoList from './TodoList'
 
@@ -11,6 +12,7 @@ root.render(
  
     <>
     <Todo/>
+    <TextInputWithFocusButton/>
     </>
 
 

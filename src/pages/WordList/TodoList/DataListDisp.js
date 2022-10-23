bc6871fad911a923185da4080b0903d05021e30e
191078834 <191078834@qq.com>
2022-10-23 20:31:57 +0900

    disinfo 完了しました

--- src/pages/WordList/TodoList/DataListDisp.js
+++ src/pages/WordList/TodoList/DataListDisp.js
@@ -13,15 +13,13 @@ export default function DataListDisp(props) {
   
   let disInfo
   let getDataInfo
+  let disInfoCheck 
+
   const [rows, setsRows] = React.useState(props.rows);
   
   let sesRows = React.useRef([]);
   const { isLoading, isError, isUpdate, isDelete, data, doPostFetch } = useWordListActionState();
-  const [alertContrl, setAlertContrl] = React.useState({
-    alertOpen: true,
-    alertSeverity: "error",
-    alertMessage: "loading failure"
-  })
+  const [alertContrl, setAlertContrl] = React.useState(false)
 
   // cellを仮更新
   const changeCell = (changeValue) => {
@@ -37,59 +35,50 @@ export default function DataListDisp(props) {
 
   //cellを削除
   const deleteCell = () => {
-    // if (sesRows.current.length === 0) {
-    //   setAlertContrl({
-    //     alertOpen: true,
-    //     alertSeverity: 'warning',
-    //     alertMessage: 'チェックしない'
-    //   });
-      // return;
+    if (sesRows.current.length === 0) {
+      setAlertContrl(true);
+      return;
     }
 
     //削除したデータ
-    // let postDeletedData = rows.filter((element) => sesRows.current.indexOf(element.id) !== -1);
-    // doPostFetch(postDeletedData);
+    setAlertContrl(false);
+    let postDeletedData = rows.filter((element) => sesRows.current.indexOf(element.id) !== -1);
+    doPostFetch(postDeletedData, "http://localhost:8090/wordlist/update");
 
     //已经被删除过多的数据表
-    // const deletedNewRows = rows.filter((element) => sesRows.current.indexOf(element.id) === -1);
+    const deletedNewRows = rows.filter((element) => sesRows.current.indexOf(element.id) === -1);
+    setsRows(deletedNewRows);
+  }
 
-    //delete message display
-  //   setAlertContrl({
-  //     alertOpen: true,
-  //     alertSeverity: 'success',
-  //     alertMessage: '削除をしました'
-  //   });
-  //   setsRows(deletedNewRows);
-  // }
   //更新ボタン
   const updateCell = () => {
     if (sesRows.current.length === 0) {
-      // setAlertContrl({
-      //   alertOpen: true,
-      //   alertSeverity: 'warning',
-      //   alertMessage: 'チェックしない'
-      // });
-      disInfo = <Alert severity="warning" variant="standard"> チェックしない</Alert>
+      setAlertContrl(true);
       return
     }
     // 仮fetchのデータ
+    setAlertContrl(false);
     let postData = rows.filter((element) => sesRows.current.indexOf(element.id) !== -1)
     doPostFetch(postData, "http://localhost:8090/wordlist/update");
     }
 
-   
+   if(alertContrl===false){
     if(props.isError){getDataInfo=<Alert severity="error" variant="standard"> データ取得失敗</Alert>}
   
     if(isUpdate&&data.data.length>0){ 
-      disInfo = <Alert severity="success" variant="standard"> データ更新しました</Alert>}
-    if(isError){disInfo = <Alert severity="error" variant="standard"> 操作失敗しました</Alert>}
+      disInfo = <Alert severity="success" variant="standard"> データ操作成功しました</Alert>}
+    if(isError&&props.isError===false){disInfo = <Alert severity="error" variant="standard"> 操作失敗しました</Alert>}
+    }
+    if(alertContrl===true){
+      disInfoCheck = <Alert severity="warning" variant="standard"> チェックしない</Alert>
+    }
 
   return (
     <Box sx={{ height: 750, width: '100%' }}>
       <Stack direction="row" spacing={3} justifyContent="flex-end">
         <Box >
           <Collapse in={true} sx={{ width: '100%',mr:150}} >
-        {disInfo}{getDataInfo}
+          {disInfoCheck}{disInfo}{getDataInfo}
           </Collapse>
           
             <Collapse in={props.isError} sx={{ width: '100%',mr:150}} >
